d16c343c298ad89e7e6ef09d17d8e82070bd4a9b
natnael <110092137+nati-kid@users.noreply.github.com>
2022-12-14 03:43:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+// console.log(nati);
+// border = document.getElementById('border')
+// console.log(border);
+
+let board;
+let playerO = "O";
+let playerx = "x";
+let currplayer = playerO;
+let gameOver = false;
+
+window.onload = function(){
+    setGame();
+}
+function setGame (){
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    for (let r = 0; r < 3; r++){
+        for(let c = 0; c < 3; c++){
+            // <div id="0-0"></div>
+            let tile = document.createElement('div');
+            tile.id= r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if(r == 0 || r == 1){
+            tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+            tile.classList.add("vertical-line");
+            }
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+function setTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (board[r][c] != ' ') { 
+        //already taken spot
+        return;
+    }
+    
+    board[r][c] = currplayer; //mark the board
+    this.innerText = currplayer; //mark the board on html
+
+    //change players
+    if (currplayer == playerO) {
+        currplayer = playerx ;
+    }
+    else {
+        currplayer = playerO;
+    }
+
+    //check winner
+    checkWinner();
+}
+
+
+function checkWinner() {
+    //horizontally, check 3 rows
+    for (let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
+            //if we found the winning row
+            //apply the winner style to that row
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(r.toString() + "-" + i.toString());
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
+
+    //vertically, check 3 columns
+    for (let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
+            //if we found the winning col
+            //apply the winner style to that col
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + c.toString());                
+                tile.classList.add("winner");
+            }
+            gameOver = true;
+            return;
+        }
+    }
+
+    //diagonally
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + "-" + i.toString());                
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
+
+    //anti-diagonally
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+        //0-2
+        let tile = document.getElementById("0-2");                
+        tile.classList.add("winner");
+
+        //1-1
+        tile = document.getElementById("1-1");                
+        tile.classList.add("winner");
+
+        //2-0
+        tile = document.getElementById("2-0");                
+        tile.classList.add("winner");
+        gameOver = true;
+        return;
+    }
+}
